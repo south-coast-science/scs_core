2ba01451f1808d627b871a3e21482c63bf8ff850
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-03-04 11:40:03 +0000

    Updated AFE class to optionally use an SHT temperature.

--- scs_core/sample/gases_datum.py
+++ scs_core/sample/gases_datum.py
@@ -18,16 +18,16 @@ class GasesDatum(SampleDatum):
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def __init__(self, rec, afe_sample, sht_sample):
+    def __init__(self, rec, afe_datum, sht_datum):
         """
         Constructor
         """
         val = []
 
-        for key in afe_sample.sns:
-            val.append((key, afe_sample.sns[key]))
+        for key in afe_datum.sns:
+            val.append((key, afe_datum.sns[key]))
 
-        val.append(('pt1', afe_sample.pt1000))
-        val.append(('sht', sht_sample))
+        val.append(('pt1', afe_datum.pt1000))
+        val.append(('sht', sht_datum))
 
         super().__init__(rec, *val)
