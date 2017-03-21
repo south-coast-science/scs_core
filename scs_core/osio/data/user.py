ed3c120f13057c9052d64aca24ea368c215e763b
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-03-21 16:15:10 +0000

    Created OSIO User class.

--- scs_core/osio/data/user.py
+++ scs_core/osio/data/user.py
@@ -31,24 +31,26 @@ class User(JSONable):
             return None
 
         id = jdict.get('id')
-        name = jdict.get('lon')
+        name = jdict.get('name')
         email = jdict.get('email')
         
         start = jdict.get('month')
 
-        return User(id, name, email, start)
+        return User(id, name, email, None, start)
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def __init__(self, id, name, email, start):
+    def __init__(self, id, name, email, password, start):
         """
         Constructor
         """
         self.__id = id                  # string
         self.__name = name              # string
         self.__email = email            # string
-        
+
+        self.__password = password      # string
+
         self.__start = start            # string (year-month)
 
 
@@ -57,13 +59,42 @@ class User(JSONable):
     def as_json(self):
         jdict = OrderedDict()
 
-        jdict['id'] = self.id
-        jdict['name'] = self.name
-        jdict['email'] = self.email
+        if self.id is not None:
+            jdict['id'] = self.id
 
-        jdict['month'] = self.start
+        if self.name is not None:
+            jdict['name'] = self.name
+
+        if self.email is not None:
+            jdict['email'] = self.email
+
+        if self.password is not None:
+            jdict['password'] = self.password
+
+        if self.start is not None:
+            jdict['month'] = self.start
 
         return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def start_year(self):
+        if self.start is None:
+            return None
+
+        year = self.start.split('-')[0]
+
+        return int(year)
+
+
+    def start_month(self):
+        if self.start is None:
+            return None
+
+        month = self.start.split('-')[1]
+
+        return int(month)
 
 
     # ----------------------------------------------------------------------------------------------------------------
@@ -84,6 +115,11 @@ class User(JSONable):
 
 
     @property
+    def password(self):
+        return self.__password
+
+
+    @property
     def start(self):
         return self.__start
 
@@ -91,4 +127,5 @@ class User(JSONable):
     # ----------------------------------------------------------------------------------------------------------------
 
     def __str__(self, *args, **kwargs):
-        return "User:{id:%s, name:%s, email:%s, start:%s}" % (self.id, self.name, self.email, self.start)
+        return "User:{id:%s, name:%s, email:%s, password:%s, start:%s}" % \
+               (self.id, self.name, self.email, self.password, self.start)
---
+++ scs_core/osio/manager/user_manager.py
@@ -0,0 +1,58 @@
+"""
+Created on 21 Mar 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_core.osio.client.rest_client import RESTClient
+from scs_core.osio.data.device import Device
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class UserManager(object):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, http_client, api_key):
+        """
+        Constructor
+        """
+        self.__rest_client = RESTClient(http_client, api_key)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def find(self, user_id, client_id):
+        path = '/v1/users/' + user_id + '/devices/' + client_id
+
+        # request...
+        self.__rest_client.connect()
+
+        try:
+            response_jdict = self.__rest_client.get(path)
+        except RuntimeError:
+            response_jdict = None
+
+        self.__rest_client.close()
+
+        device = Device.construct_from_jdict(response_jdict)
+
+        return device
+
+
+    def find_members_of_org(self, org_id):
+        pass
+
+
+    def update(self):
+        pass
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "UserManager:{rest_client:%s}" % self.__rest_client
