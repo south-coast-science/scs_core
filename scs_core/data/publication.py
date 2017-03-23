4aa98ebeb102a54b885fb654b2df6faa2fef452b
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-03-23 07:44:18 +0000

    Created OSIO Publication class.

---
+++ scs_core/data/publication.py
@@ -0,0 +1,74 @@
+"""
+Created on 22 Mar 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+example:
+{
+    "topic": 28,
+    "payload": "south-coast-science-gases-Ox-NO2-NO-CO",
+}
+"""
+
+from collections import OrderedDict
+
+from scs_core.data.json import JSONable
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class Publication(JSONable):
+    """
+    classdocs
+   """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def construct_from_jdict(cls, jdict):
+        if not jdict:
+            return None
+
+        topic = jdict.get('topic')
+        payload = jdict.get('payload')
+
+        return Publication(topic, payload)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, topic, payload):
+        """
+        Constructor
+        """
+        self.__topic = topic                    # string
+        self.__payload = payload                # string
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        jdict = OrderedDict()
+
+        jdict['topic'] = self.topic
+        jdict['payload'] = self.payload
+
+        return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def topic(self):
+        return self.__topic
+
+
+    @property
+    def payload(self):
+        return self.__payload
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "Publication:{topic:%s, payload:%s}" % (self.topic, self.payload)
