53d10d27192eeeafa7a31223431a0605e6d21d58
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-04-24 08:21:01 +0100

    Moved gas data structures from scs_dfe to scs_core.

---
+++ scs_core/gas/__init__.py
@@ -0,0 +1,12 @@
+"""
+Created on 24 Apr 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_core.gas.temp_comp import TempComp
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+TempComp.init()     # must be initialised before sensors
