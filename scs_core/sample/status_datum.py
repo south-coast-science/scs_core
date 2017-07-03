fac32e6990c7f0d32ec5c292ce949dffd3f48054
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-07-03 12:16:00 +0100

    Added uptime to StatusDatum.

--- scs_core/sample/status_datum.py
+++ scs_core/sample/status_datum.py
@@ -16,8 +16,8 @@ class StatusDatum(SampleDatum):
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def __init__(self, tag, rec, location, temperature, exception):
+    def __init__(self, tag, rec, location, temperature, uptime):
         """
         Constructor
         """
-        super().__init__(tag, rec, ('loc', location), ('tmp', temperature), ('exc', exception))
+        super().__init__(tag, rec, ('loc', location), ('tmp', temperature), ('up', uptime))
