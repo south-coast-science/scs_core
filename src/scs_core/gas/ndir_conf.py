bd35930e2ad0908ea68038e9441edb85c5fbc320
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-03-15 15:15:28 +0000

    Refactored Tx NDIR structure.

--- src/scs_core/gas/ndir_conf.py
+++ src/scs_core/gas/ndir_conf.py
@@ -7,8 +7,10 @@ A stub class for an NDIRConf that may be implemented elsewhere
 """
 
 from abc import abstractmethod
+from collections import OrderedDict
 
 from scs_core.data.json import PersistentJSONable
+from scs_core.gas.ndir_monitor import NDIRMonitor
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -18,26 +20,63 @@ class NDIRConf(PersistentJSONable):
     classdocs
     """
 
+    _FILENAME = "ndir_conf.json"
+
     @classmethod
     def filename(cls, host):
-        return None
+        return None             # prevent instances of this class from constructing real NDIRs
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, model, tally):
+        """
+        Constructor
+        """
+        super().__init__()
+
+        self.__model = model
+        self.__tally = tally
 
 
     # ----------------------------------------------------------------------------------------------------------------
     # abstract NDIRConf...
 
     @abstractmethod
+    def ndir(self, host):
+        pass
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
     def ndir_monitor(self, host):
-        pass
+        return NDIRMonitor(self.ndir(host), self)
 
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        jdict = OrderedDict()
+
+        jdict['model'] = self.model
+        jdict['tally'] = self.tally
+
+        return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
 
     @property
-    @abstractmethod
     def model(self):
-        pass
+        return self.__model
 
 
     @property
-    @abstractmethod
     def tally(self):
-        pass
+        return self.__tally
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "NDIRConf:{model:%s, tally:%s}" %  (self.model, self.tally)
