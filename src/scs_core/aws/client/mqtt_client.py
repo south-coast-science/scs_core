1466a101da3a5177f01ffb2318352b1c814ac974
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-08-02 10:48:52 +0100

    Stabilised AWS MQTT client.

--- src/scs_core/aws/client/mqtt_client.py
+++ src/scs_core/aws/client/mqtt_client.py
@@ -23,7 +23,7 @@ class MQTTClient(object):
     classdocs
     """
 
-    __KEEP_ALIVE_INTERVAL =         600                     # seconds
+    __KEEP_ALIVE_INTERVAL =         600                     # recommended: 600 (sec)
 
     __PORT =                        8883
 
