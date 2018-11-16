d3f355f8980a4e160949c0abf6fc9f71339a0416
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-11-16 09:03:43 +0000

    Developed OPCN3 class.

--- tests/data/linear_regression_test.py
+++ tests/data/linear_regression_test.py
@@ -13,13 +13,13 @@ from scs_core.data.path_dict import PathDict
 
 # --------------------------------------------------------------------------------------------------------------------
 
-j1 = '{"rec": "2016-10-14T14:19:15.677+01:00", "val": {"opc": {"pm1": 1.5, "pm2p5": 2.7, "pm10": 3.3, "per": 5.0, ' \
+j1 = '{"rec": "2016-10-14T14:19:15.677+01:00", "val": {"opc_n2": {"pm1": 1.5, "pm2p5": 2.7, "pm10": 3.3, "per": 5.0, ' \
      '"bin": [95, 30, 11, 1, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0], "mtf1": 31, "mtf3": 29, "mtf5": 0, "mtf7": 0}}}'
 
-j2 = '{"rec": "2016-10-14T14:19:20.680+01:00", "val": {"opc": {"pm1": 1.8, "pm2p5": 4.3, "pm10": 6.9, "per": 5.0, ' \
+j2 = '{"rec": "2016-10-14T14:19:20.680+01:00", "val": {"opc_n2": {"pm1": 1.8, "pm2p5": 4.3, "pm10": 6.9, "per": 5.0, ' \
      '"bin": [76, 36, 19, 6, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], "mtf1": 26, "mtf3": 35, "mtf5": 26, "mtf7": 35}}}'
 
-j3 = '{"rec": "2016-10-14T14:19:25.680+01:00", "val": {"opc": {"pm1": 0.9, "pm2p5": 1.7, "pm10": 22.4, "per": 5.0, ' \
+j3 = '{"rec": "2016-10-14T14:19:25.680+01:00", "val": {"opc_n2": {"pm1": 0.9, "pm2p5": 1.7, "pm10": 22.4, "per": 5.0, ' \
      '"bin": [62, 23, 8, 2, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0], "mtf1": 20, "mtf3": 24, "mtf5": 0, "mtf7": 0}}}'
 
 
@@ -34,7 +34,7 @@ print(d1)
 print("-")
 
 rec = LocalizedDatetime.construct_from_iso8601(d1.node('rec'))
-val = d1.node('val.opc.pm10')
+val = d1.node('val.opc_n2.pm10')
 
 lr.append(rec, val)
 print(lr)
@@ -45,7 +45,7 @@ print(d2)
 print("-")
 
 rec = LocalizedDatetime.construct_from_iso8601(d2.node('rec'))
-val = d2.node('val.opc.pm10')
+val = d2.node('val.opc_n2.pm10')
 
 lr.append(rec, val)
 print(lr)
@@ -56,7 +56,7 @@ print(d3)
 print("-")
 
 rec = LocalizedDatetime.construct_from_iso8601(d3.node('rec'))
-val = d3.node('val.opc.pm10')
+val = d3.node('val.opc_n2.pm10')
 
 lr.append(rec, val)
 print(lr)
--- tests/data/path_dict_root_test.py
+++ tests/data/path_dict_root_test.py
@@ -16,7 +16,7 @@ from scs_core.data.path_dict import PathDict
 
 # --------------------------------------------------------------------------------------------------------------------
 
-jstr = '{"rec": "2016-09-27T13:29:52.947+01:00", "val": {"opc": {"pm1": 5.1, "pm2p5": 22.7, "pm10": 195.1, ' \
+jstr = '{"rec": "2016-09-27T13:29:52.947+01:00", "val": {"opc_n2": {"pm1": 5.1, "pm2p5": 22.7, "pm10": 195.1, ' \
        '"per": 5.0, "bin1": [77, 38, 52, 39, 9, 15, 9, 2], "bin2": [4, 1, 2, 4, 0, 0, 0, 0], ' \
        '"mtf1": 30, "mtf3": 34, "mtf5": 34, "mtf7": 39}}}'
 print(jstr)
@@ -47,15 +47,15 @@ target.copy(datum)
 print(target)
 print("-")
 
-path1 = "val.opc.bin1.0"
+path1 = "val.opc_n2.bin1.0"
 print(path1)
 print("-")
 
-path2 = "val.opc.bin2"
+path2 = "val.opc_n2.bin2"
 print(path2)
 print("-")
 
-path3 = "val.opc.bin1.1"
+path3 = "val.opc_n2.bin1.1"
 print(path3)
 print("-")
 
@@ -63,11 +63,11 @@ target.copy(datum, path1)
 print(target)
 print("-")
 
-source = "val.opc.extra"
+source = "val.opc_n2.extra"
 print(source)
 print("-")
 
-target.append("val.opc.extra", "hello")
+target.append("val.opc_n2.extra", "hello")
 print(target)
 print("-")
 
