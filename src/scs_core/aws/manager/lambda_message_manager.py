84eca55f09664e5b91c91817d46980a459e4cfb7
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-11-26 18:01:17 +0000

    Added AWS lambda version of MessageManager.

---
+++ src/scs_core/aws/manager/lambda_message_manager.py
@@ -0,0 +1,139 @@
+"""
+Created on 26 Nov 2018
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+This version of MessageManager for AWS lambda function with multi-part response.
+
+https://xy1eszuu23.execute-api.us-west-2.amazonaws.com/staging/topicMessages?
+topic=south-coast-science-dev/production-test/loc/1/gases&
+startTime=2018-11-23T14:37:10.393Z&
+endTime=2018-11-23T15:10:59.712Z"
+"""
+
+from urllib.parse import urlparse, parse_qs
+
+from scs_core.aws.client.rest_client import RESTClient
+from scs_core.aws.data.message import Message
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class MessageManager(object):
+    """
+    classdocs
+    """
+
+    __TOPIC =       'topic'
+    __START =       'startTime'
+    __END =         'endTime'
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, http_client, api_key, reporter=None):
+        """
+        Constructor
+        """
+        self.__rest_client = RESTClient(http_client, api_key)
+        self.__reporter = reporter
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def find_for_topic(self, topic, start_date, end_date):
+        # request_path = '/' + topic + '/' + start_date.utc().as_iso8601() + '/' + end_date.utc().as_iso8601()
+
+        request_path = '/staging/topicMessages'
+
+        params = {self.__TOPIC: topic, self.__START: start_date.utc().as_iso8601(),
+                  self.__END: end_date.utc().as_iso8601()}
+
+        # request...
+        self.__rest_client.connect()
+
+        try:
+            while True:
+                jdict = self.__rest_client.get(request_path, params)
+
+                # messages...
+                block = MessageResponse.construct_from_jdict(jdict)
+
+                for item in block.items:
+                    yield item
+
+                # progress...
+                if self.__reporter:
+                    self.__reporter.print(params[self.__START], len(block))
+
+                # next request...
+                if block.next_url is None:
+                    break
+
+                next_url = urlparse(block.next_url)
+                next_params = parse_qs(next_url.query)
+
+                params[self.__START] = next_params[self.__START][0]
+
+        finally:
+            self.__rest_client.close()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "MessageManager:{rest_client:%s, reporter:%s}" % (self.__rest_client, self.__reporter)
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class MessageResponse(object):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def construct_from_jdict(cls, jdict):
+        if not jdict:
+            return None
+
+        items = [Message.construct_from_jdict(msg_jdict) for msg_jdict in jdict.get('Items')]
+        next_url = jdict.get('next')
+
+        return MessageResponse(items, next_url)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, items, next_url):
+        """
+        Constructor
+        """
+        self.__items = items                        # list of Message
+        self.__next_url = next_url                  # URL string
+
+
+    def __len__(self):
+        return len(self.items)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def items(self):
+        return self.__items
+
+
+    @property
+    def next_url(self):
+        return self.__next_url
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        items = '[' + ', '.join(str(item) for item in self.items) + ']'
+
+        return "MessageResponse:{items:%s, next_url:%s}" % (items, self.next_url)
