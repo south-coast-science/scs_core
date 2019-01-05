7e5f7837e45b6f30c383375f84826762fc18ff46
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-01-05 12:06:00 +0000

    Cleaning.

--- src/scs_core/sample/status_sample.py
+++ src/scs_core/sample/status_sample.py
@@ -4,16 +4,13 @@ Created on 20 Oct 2016
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
 example document:
-{"tag": "scs-bgx-401",
- "rec": "2017-09-24T07:56:27.304+00:00",
- "val": {
-  "tz": {"name": "Europe/London", "utc-offset": "+01:00"},
-  "pos": {"lat": 50.8229247, "lng": -0.1229787, "qual": 2},
-  "sch": {"scs-climate": {"interval": 60.0, "tally": 1}, "scs-gases": {"interval": 10.0, "tally": 1},
-    "scs-particulates": {"interval": 10.0, "tally": 1}, "scs-status": {"interval": 60.0, "tally": 1}},
-  "tmp": {"brd": 35.8, "hst": null},
-  "up": {"period": "01-21:52:00.000", "users": 0, "load": {"av1": 0.0, "av5": 0.0, "av15": 0.0}},
-  "psu": {"p-rst": false, "w-rst": false, "batt-flt": false, "host-3v3": 3.4, "pwr-in": 12.4, "prot-batt": 6.7}}}
+{"tag": "scs-bgx-500", "rec": "2019-01-05T12:04:10Z", "val": {"tz": {"name": "Europe/London", "utc-offset": "+00:00"},
+"gps": {"pos": [50.8230166, -0.1229759], "elv": 39.9, "qual": 1},
+"sch": {"scs-climate": {"interval": 60.0, "tally": 1}, "scs-gases": {"interval": 10.0, "tally": 1},
+"scs-particulates": {"interval": 10.0, "tally": 1}, "scs-status": {"interval": 60.0, "tally": 1}},
+"tmp": {"brd": 29.6}, "up": {"period": "00-00:31:00", "users": 1, "load": {"av1": 0.06, "av5": 0.07, "av15": 0.22}},
+"psu": {"rst": "00", "standby": false, "chg": "0000", "batt-flt": false, "host-3v3": 3.4, "pwr-in": 15.8,
+"prot-batt": 0.1}}}
 """
 
 from scs_core.sample.sample import Sample
