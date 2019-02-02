596fc663f2be59cadd4788f208647f178e85700d
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-02-02 12:42:21 +0000

    Added environmental parameters to AFEBaseline.

--- src/scs_core/gas/sensor_baseline.py
+++ src/scs_core/gas/sensor_baseline.py
@@ -10,8 +10,8 @@ example JSON:
 from collections import OrderedDict
 
 from scs_core.data.datum import Datum
-from scs_core.data.localized_datetime import LocalizedDatetime
 from scs_core.data.json import JSONable
+from scs_core.data.localized_datetime import LocalizedDatetime
 
 
 # --------------------------------------------------------------------------------------------------------------------
