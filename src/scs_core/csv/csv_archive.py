a3db6f63a5514a5fdff4aaec4481ace008b85e35
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-03-06 17:09:22 +0000

    Replaced PK ValueErrors with KeyErrors.

--- src/scs_core/csv/csv_archive.py
+++ src/scs_core/csv/csv_archive.py
@@ -31,7 +31,7 @@ class CSVArchive(ABC):
                 instance = cls.construct_from_jdict(json.loads(row))
 
                 if instance.pk in cls._retrieved:
-                    raise ValueError("duplicate pk '%s' for instance:%s" % (instance.pk, instance))
+                    raise KeyError("duplicate pk '%s' for instance:%s" % (instance.pk, instance))
 
                 cls._retrieved[instance.pk] = instance
 
@@ -48,9 +48,6 @@ class CSVArchive(ABC):
     def instance(cls, pk):
         if pk is None:
             return None
-
-        if pk not in cls._retrieved:
-            raise ValueError(pk)
 
         return cls._retrieved[pk]
 
