26f5c3a58cdc3a989e0a29be68da5ceff4cf13e1
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-04-17 16:23:44 +0100

    Stats development updates.

--- src/scs_core/data/error_grid.py
+++ src/scs_core/data/error_grid.py
@@ -6,6 +6,8 @@ Created on 16 Apr 2019
 
 from collections import OrderedDict
 
+import scipy.stats
+
 from scs_core.data.error_sample import ErrorSample
 from scs_core.data.json import JSONable
 
@@ -127,7 +129,7 @@ class ErrorGridRhT(JSONable):
 
             while min_rh < self.grid_max_rh:
                 cell = self._cells[max_rh][max_t]
-                prefix = 'rh_' + str(max_rh)
+                prefix = 'rH_' + str(max_rh)
 
                 jdict[prefix + '_n'] = len(cell)
                 jdict[prefix + '_stdev'] = cell.stdev()
@@ -209,8 +211,8 @@ class ErrorGridTRh(ErrorGridRhT):
             jdict = OrderedDict()
 
             # rH cols...
-            jdict['rh_max'] = max_rh
-            jdict['rh_avg'] = round((min_rh + max_rh) / 2, 1)
+            jdict['rH_max'] = max_rh
+            jdict['rH_avg'] = round((min_rh + max_rh) / 2, 1)
 
             # t cols...
             min_t = self.grid_min_t
@@ -237,5 +239,98 @@ class ErrorGridTRh(ErrorGridRhT):
 
     def __str__(self, *args, **kwargs):
         return "ErrorGridTRh:{min_rh:%s, max_rh:%s, rh_step:%s, min_t:%s, max_t:%s, t_step:%s, t deltas:%s}" % \
+               (self.grid_min_rh, self.grid_max_rh, self.rh_step, self.grid_min_t, self.grid_max_t, self.t_step,
+                len(self))
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class ErrorGridStats(ErrorGridRhT):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, grid_min_rh, grid_max_rh, rh_step, grid_min_t, grid_max_t, t_step, cells):
+        """
+        Constructor
+        """
+        super().__init__(grid_min_rh, grid_max_rh, rh_step, grid_min_t, grid_max_t, t_step, cells)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        # rH rows...
+        min_rh = self.grid_min_rh
+        max_rh = min_rh + self.rh_step
+
+        while min_rh < self.grid_max_rh:
+            count, col_t_min, col_t_max, means, samples = self.__t_stats(max_rh)
+
+            # stats...
+            slope, intercept, r, p, std_err = scipy.stats.linregress(means, samples)
+
+            jdict = OrderedDict()
+
+            # rH cols...
+            jdict['rH_max'] = max_rh
+            jdict['rH_avg'] = round((min_rh + max_rh) / 2, 1)
+
+            jdict['t_min'] = col_t_min
+            jdict['t_max'] = col_t_max
+
+            jdict['n'] = count
+
+            jdict['mT'] = round(slope, 3)
+            jdict['cT'] = round(intercept, 3)
+            jdict['r2'] = round(r ** 2, 3)
+
+            yield jdict
+
+            min_rh = max_rh
+            max_rh += self.rh_step
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __t_stats(self, max_rh):
+        count = 0
+        col_min_t = None
+        col_max_t = None
+
+        means = []
+        samples = []
+
+        # t cols...
+        min_t = self.grid_min_t
+        max_t = min_t + self.t_step
+
+        while min_t < self.grid_max_t:
+            cell = self._cells[max_rh][max_t]
+
+            if len(cell) > 0:
+                means.append(round((min_t + max_t) / 2, 1))
+                samples.append(cell.avg())
+
+                if col_min_t is None:
+                    col_min_t = min_t
+
+                if col_max_t is None or max_t > col_max_t:
+                    col_max_t = max_t
+
+                count += len(cell)
+
+            min_t = max_t
+            max_t += self.t_step
+
+        return count, col_min_t, col_max_t, means, samples
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "ErrorGridStats:{min_rh:%s, max_rh:%s, rh_step:%s, min_t:%s, max_t:%s, t_step:%s, t deltas:%s}" % \
                (self.grid_min_rh, self.grid_max_rh, self.rh_step, self.grid_min_t, self.grid_max_t, self.t_step,
                 len(self))
