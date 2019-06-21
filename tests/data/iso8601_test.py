d0a9c0dc3b4c162c037cd9ae0eec353ed9db0a98
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-06-21 11:33:25 +0100

    Added SystemDisplay class.

---
+++ tests/data/iso8601_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Created on 21 Jun 2019
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_core.data.localized_datetime import LocalizedDatetime, ISO8601
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+now = LocalizedDatetime.now()
+print(now)
+print("-")
+
+iso = ISO8601.construct(now)
+print(iso)
+print("-")
+
+print("date: %s" % iso.date)
+print("time: %s" % iso.time)
+print("time_secs: %s" % iso.time_secs)
+print("time_millis: %s" % iso.time_millis)
+print("timezone: %s" % iso.timezone)
