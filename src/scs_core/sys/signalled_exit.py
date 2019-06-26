e8ac72552d09854e7d1ffeb64833cf6981502664
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-06-26 17:27:46 +0100

    Added SignalledExit class.

---
+++ src/scs_core/sys/signalled_exit.py
@@ -0,0 +1,73 @@
+"""
+Created on 26 Jun 2019
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+import signal
+import sys
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# noinspection PyUnusedLocal
+
+class SignalledExit(object):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, client, verbose):
+        """
+        Constructor
+        """
+        self.__client = client                                                  # string
+        self.__verbose = verbose                                                # bool
+
+        self.__exiting = False                                                  # bool
+        self.__original_sigint_handler = signal.getsignal(signal.SIGINT)        # function
+
+        signal.signal(signal.SIGINT, self.sigint_handler)
+        signal.signal(signal.SIGTERM, self.sigterm_handler)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def sigint_handler(self, signum, frame):
+        if self.__exiting:
+            return
+
+        self.clear()
+        self.__exiting = True
+
+        if self.__verbose:
+            print("%s: SIGINT" % self.__client, file=sys.stderr)
+
+        sys.exit(1)
+
+
+    def sigterm_handler(self, signum, frame):
+        if self.__exiting:
+            return
+
+        self.clear()
+        self.__exiting = True
+
+        if self.__verbose:
+            print("%s: SIGTERM" % self.__client, file=sys.stderr)
+
+        sys.exit(0)
+
+
+    def clear(self):
+        signal.signal(signal.SIGINT, self.__original_sigint_handler)
+        signal.signal(signal.SIGTERM, signal.SIG_IGN)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "SignalledExit:{client:%s, verbose:%s, exiting:%s, original_sigint_handler:%s}" % \
+               (self.__client, self.__verbose, self.__exiting, self.__original_sigint_handler)
---
+++ tests/sys/signalled_exit_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Created on 26 Jun 2019
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+import time
+
+from scs_core.sys.signalled_exit import SignalledExit
+
+
+# --------------------------------------------------------------------------------------------------------------------
+# run...
+
+
+listener = SignalledExit("test", True)
+print(listener)
+
+while True:
+    time.sleep(1)
+
