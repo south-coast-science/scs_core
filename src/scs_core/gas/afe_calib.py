6a4d829552454944a8ad3d6e489e0bab28339c12
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-09-16 13:41:55 +0100

    Updated AFECalib DSI_WRAPPER.

--- src/scs_core/gas/afe_calib.py
+++ src/scs_core/gas/afe_calib.py
@@ -71,8 +71,8 @@ class AFECalib(PersistentJSONable):
                 {"serial_number": "00-000000", "type": "000-0000-00", "calibrated_on": "YYYY-MM-DD", 
                 "dispatched_on": null, "pt1000_v20": 1.0, 
                 "sn1": {"serial_number": "NNNNNNNNN", "sensor_type": "A4", "we_electronic_zero_mv": 300, 
-                "we_sensor_zero_mv": 6, "we_total_zero_mv": 300, "ae_electronic_zero_mv": 300, 
-                "ae_sensor_zero_mv": 1, "ae_total_zero_mv": 300, "we_sensitivity_na_ppb": "S.SSSSSSS", 
+                "we_sensor_zero_mv": 0, "we_total_zero_mv": 300, "ae_electronic_zero_mv": 300, 
+                "ae_sensor_zero_mv": 0, "ae_total_zero_mv": 300, "we_sensitivity_na_ppb": "S.SSSSSSS", 
                 "we_cross_sensitivity_no2_na_ppb": -0.3, "pcb_gain": -0.7, "we_sensitivity_mv_ppb": 0.2, 
                 "we_cross_sensitivity_no2_mv_ppb": 0.2}}
                   '''
