8c0dc0db0988828892d0aededbb4c4a0f2bbafdf
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-09-16 14:38:10 +0100

    Reverted to AWS recommended MQTT client timings.

--- src/scs_core/aws/client/mqtt_client.py
+++ src/scs_core/aws/client/mqtt_client.py
@@ -38,7 +38,7 @@ class MQTTClient(object):
     __RECONN_STABLE =               20                      # recommended: 20 (sec), was 10
 
     __DISCONNECT_TIMEOUT =          10                      # recommended: 10 (sec), was 40
-    __OPERATION_TIMEOUT =           5                      # recommended: 5 (sec), was 20
+    __OPERATION_TIMEOUT =           5                       # recommended: 5 (sec), was 20
 
     __PUB_QOS =                     1
     __SUB_QOS =                     1
