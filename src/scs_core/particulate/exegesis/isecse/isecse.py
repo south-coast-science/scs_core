5c9bced20c567aa7ac1a3178d4d9c094a33ab7da
Bruno Beloff <bruno.beloff@southcoastscience.com>
2019-11-12 08:57:02 +0000

    Added ISECSEN2v2 particulate exegesis

---
+++ src/scs_core/particulate/exegesis/isecse/isecse.py
@@ -0,0 +1,143 @@
+"""
+Created on 11 Nov 2019
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+method: Immediate Scaling Error / Curve is Single Exponential (ISECSE), OPC-N2, version 1
+
+domain: 0 <= rH <= max_rh
+model: error = ce * e ^ (cx * rH)
+range: PM / error
+"""
+
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+from math import exp
+
+from scs_core.data.json import PersistentJSONable
+from scs_core.particulate.exegesis.text import Text
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class ISECSE(PersistentJSONable, ABC):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    @abstractmethod
+    def name(cls):
+        pass
+
+
+    @classmethod
+    def persistence_location(cls, host):
+        return host.conf_dir(), "particulate_exegete_" + cls.name() + "_calib.json"
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def construct_from_jdict(cls, jdict):
+        if not jdict:
+            return cls.standard()
+
+        ce = jdict.get('ce')
+        cx = jdict.get('cx')
+
+        max_rh_pm1 = jdict.get('max-rh-pm1')
+        max_rh_pm2p5 = jdict.get('max-rh-pm2p5')
+        max_rh_pm10 = jdict.get('max-rh-pm10')
+
+        return cls(ce, cx, max_rh_pm1, max_rh_pm2p5, max_rh_pm10)
+
+
+    @classmethod
+    @abstractmethod
+    def standard(cls):
+        pass
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, ce, cx, max_rh_pm1, max_rh_pm2p5, max_rh_pm10):
+        """
+        Constructor
+        """
+        super().__init__()
+
+        self.__ce = ce                                      # coefficient of e              dict of float
+        self.__cx = cx                                      # coefficient of x              dict of float
+
+        self.__max_rh_pm1 = int(max_rh_pm1)                 # maximum rH for PM1            int
+        self.__max_rh_pm2p5 = int(max_rh_pm2p5)             # maximum rH for PM2.5          int
+        self.__max_rh_pm10 = int(max_rh_pm10)               # maximum rH for PM10           int
+
+
+    def __eq__(self, other):
+        return self.__ce == other.__ce and \
+               self.__cx == other.__cx and \
+               self.__max_rh_pm1 == other.__max_rh_pm1 and \
+               self.__max_rh_pm2p5 == other.__max_rh_pm2p5 and \
+               self.__max_rh_pm10 == other.__max_rh_pm10
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def interpret(self, datum, rh):
+        pm1 = self.__interpret('pm1', datum.pm1, rh, self.__max_rh_pm1)
+        pm2p5 = self.__interpret('pm2p5', datum.pm2p5, rh, self.__max_rh_pm2p5)
+        pm10 = self.__interpret('pm10', datum.pm10, rh, self.__max_rh_pm10)
+
+        return Text(pm1, pm2p5, pm10)
+
+
+    def tag(self):
+        if self == self.standard():
+            return self.name()
+
+        return self.name() + '?'                            # indicates non-standard coefficients
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __interpret(self, species, pm, rh, max_rh):
+        if pm is None or rh is None:
+            return None
+
+        if rh > max_rh:
+            return None
+
+        return pm / self.__error(species, rh)
+
+
+    def __error(self, species, rh):
+        ce = self.__ce[species]
+        cx = self.__cx[species]
+
+        return ce * exp(cx * rh)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        jdict = OrderedDict()
+
+        jdict['ce'] = self.__ce
+        jdict['cx'] = self.__cx
+
+        jdict['max-rh-pm1'] = self.__max_rh_pm1
+        jdict['max-rh-pm2p5'] = self.__max_rh_pm2p5
+        jdict['max-rh-pm10'] = self.__max_rh_pm10
+
+        return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return self.__class__.__name__ + ":{ce:%s, cx:%s, max_rh_pm1:%s, max_rh_pm2p5:%s, max_rh_pm10:%s}" % \
+               (self.__ce, self.__cx, self.__max_rh_pm1, self.__max_rh_pm2p5, self.__max_rh_pm10)
--- src/scs_core/particulate/exegesis/isecse/isecse_n2_v002.py
+++ src/scs_core/particulate/exegesis/isecse/isecse_n2_v002.py
@@ -1,12 +1,12 @@
 """
-Created on 26 Oct 2019
+Created on 11 Nov 2019
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
 THIS CODE SHALL BE TREATED AS IMMUTABLE. THUS, ANY ALTERATIONS TO EQUATIONS OR STANDARD COEFFICIENTS SHALL BE
 PRESENTED AS A NEW CLASS, WITH AN INCREMENTED CLASS VERSION NUMBER.
 
-Coefficients gained from Alphasense OPC-N2 (versus Palas Fidas) data at LHR2 in 2019.
+Coefficients gained from Alphasense OPC-N2 (versus Palas Fidas) data at LHR2 in 2019 (Brian's analysis)
 
 method: Immediate Scaling Error / Curve is Single Exponential (ISECSE), OPC-N2, version 1
 
@@ -22,22 +22,22 @@ from scs_core.particulate.exegesis.isecse.isecse import ISECSE
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class ISECSEN2v1(ISECSE):
+class ISECSEN2v2(ISECSE):
     """
     classdocs
     """
 
-    __NAME =                            "isecsen2v1"
+    __NAME =                            "isecsen2v2"
 
     __STANDARD_CE = OrderedDict()
-    __STANDARD_CE['pm1'] =              0.44
-    __STANDARD_CE['pm2p5'] =            0.44
-    __STANDARD_CE['pm10'] =             0.44
+    __STANDARD_CE['pm1'] =              0.3793
+    __STANDARD_CE['pm2p5'] =            0.3802
+    __STANDARD_CE['pm10'] =             0.1886
 
     __STANDARD_CX = OrderedDict()
-    __STANDARD_CX['pm1'] =              0.027
-    __STANDARD_CX['pm2p5'] =            0.027
-    __STANDARD_CX['pm10'] =             0.027
+    __STANDARD_CX['pm1'] =              0.0254
+    __STANDARD_CX['pm2p5'] =            0.0276
+    __STANDARD_CX['pm10'] =             0.0393
 
     __STANDARD_MAX_rH_PM1 =             100
     __STANDARD_MAX_rH_PM2p5 =           100
