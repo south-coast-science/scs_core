f78f9a94df1c151f0172cc5ac91965356f2a2b52
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-04-21 13:47:26 +0100

    Removed TODOs

--- src/scs_core/osio/manager/topic_manager.py
+++ src/scs_core/osio/manager/topic_manager.py
@@ -218,7 +218,7 @@ class TopicManager(object):
         self.__rest_client.connect(timeout=self.__TIMEOUT)
 
         try:
-            self.__rest_client.put(request_path, topic.as_json())       # TODO: check what it looks like
+            self.__rest_client.put(request_path, topic.as_json())
 
         except socket.timeout:
             pass
