9598e166f5ca8f86f4d53d5db3343aa98f8ad48d
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-05-27 11:19:29 +0100

    Removed TODO

--- src/scs_core/csv/csv_log_cursor_queue.py
+++ src/scs_core/csv/csv_log_cursor_queue.py
@@ -203,8 +203,6 @@ class CSVLogCursor(JSONable):
             print("CSVLogCursor: %s: %s" % (log_file.path(), ex), file=sys.stderr)
             sys.stderr.flush()
 
-            # TODO: return what we have so far?
-
             return None                         # skip corrupt files
 
         finally:
