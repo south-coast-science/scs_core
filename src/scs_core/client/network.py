8e207cfe399077767bef898111ea66dcacd7956b
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-09-28 19:36:31 +0100

    Cleaning

--- src/scs_core/client/network.py
+++ src/scs_core/client/network.py
@@ -7,17 +7,27 @@ Created on 27 Sep 2020
 import os
 import time
 
+from abc import ABC
 from subprocess import Popen
 
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class Network(object):
+class Network(ABC):
     """
     classdocs
     """
     __TEST_RESOURCE =       'google.com'
     __NETWORK_WAIT_TIME =   5.0                         # seconds
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def resource_is_available(resource):
+        with open(os.devnull, 'wb') as devnull:
+            return Popen(['ping', '-c', '1', resource], stdout=devnull, stderr=devnull).wait() == 0
+
 
     # ----------------------------------------------------------------------------------------------------------------
 
@@ -35,9 +45,3 @@ class Network(object):
     def wait_for_resource(cls, resource):
         while not cls.resource_is_available(resource):
             time.sleep(cls.__NETWORK_WAIT_TIME)
-
-
-    @classmethod
-    def resource_is_available(cls, resource):
-        with open(os.devnull, 'wb') as devnull:
-            return Popen(['ping', '-c', '1', resource], stdout=devnull, stderr=devnull).wait() == 0
