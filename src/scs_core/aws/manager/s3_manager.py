19e49c7af4d823dd5124350073068db4f780ac4b
jade <jadempage@outlook.com>
2020-09-29 12:22:42 +0100

    added ability to list all objects in a certain bucket

--- src/scs_core/aws/manager/s3_manager.py
+++ src/scs_core/aws/manager/s3_manager.py
@@ -22,7 +22,7 @@ class S3Manager(object):
 
     def list_buckets(self):
         # Retrieve the list of existing buckets
-        response = self.__client.list_buckets()
+        response = self.__client.get_list()
         bucket_list = PathDict()
         # Output the bucket names
         inters = 0
@@ -44,3 +44,10 @@ class S3Manager(object):
     def upload_file_to_bucket(self, bucket_name, filepath, object_name):
         self.__resource_client.Bucket(bucket_name).upload_file(filepath, object_name)
         return "Done"
+
+    def list_bucket_objects(self, bucket_name):
+        response = self.__client.list_objects_v2(
+            Bucket=bucket_name,
+            Delimiter=",",
+        )
+        return response
