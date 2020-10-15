0f55ba69949ed1130a2ddf19ffbf06b12d04fcc1
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-10-15 12:25:18 +0100

    Added is-available field to DiskVolume

--- src/scs_core/sys/disk_volume.py
+++ src/scs_core/sys/disk_volume.py
@@ -4,12 +4,13 @@ Created on 14 Oct 2020
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
 JSON example:
-{"filesystem": "/dev/mmcblk0p1", "size": 15384184, "used": 319296, "available": 14892092, "use-percent": 3.0,
-"mounted-on": "/srv/SCS_logging"}
+{"filesystem": "/dev/mmcblk0p1", "size": 15384184, "used": 319296, "free": 14892092,
+"mounted-on": "/srv/SCS_logging", "is-available": false}
 
 https://stackoverflow.com/questions/35469685/in-python-how-do-i-get-a-list-of-all-partitions-in-mac-os-x
 """
 
+import os
 import re
 
 from collections import OrderedDict
@@ -36,47 +37,24 @@ class DiskVolume(JSONable):
         groups = match.groups()
 
         filesystem = groups[0]
-
         size = groups[1]
         used = groups[2]
-        available = groups[3]
-        use_percent = groups[4]
-
+        free = groups[3]
         mounted_on = groups[5]
 
-        return cls(filesystem, size, used, available, use_percent, mounted_on)
-
-
-    @classmethod
-    def construct_from_jdict(cls, jdict):
-        if not jdict:
-            return None
-
-        filesystem = jdict.get('filesystem')
-
-        size = jdict.get('size')
-        used = jdict.get('used')
-        available = jdict.get('available')
-        use_percent = jdict.get('use-percent')
-
-        mounted_on = jdict.get('mounted-on')
-
-        return cls(filesystem, size, used, available, use_percent, mounted_on)
+        return cls(filesystem, size, used, free, mounted_on)
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def __init__(self, filesystem, size, used, available, use_percent, mounted_on):
+    def __init__(self, filesystem, size, used, free, mounted_on):
         """
         Constructor
         """
         self.__filesystem = filesystem                      # string
-
         self.__size = int(size)                             # int blocks
         self.__used = int(used)                             # int blocks
-        self.__available = int(available)                   # int blocks
-        self.__use_percent = float(use_percent)             # float %
-
+        self.__free = int(free)                             # int blocks
         self.__mounted_on = mounted_on                      # string
 
 
@@ -86,15 +64,26 @@ class DiskVolume(JSONable):
         jdict = OrderedDict()
 
         jdict['filesystem'] = self.filesystem
-
         jdict['size'] = self.size
         jdict['used'] = self.used
-        jdict['available'] = self.available
-        jdict['use-percent'] = self.use_percent
-
+        jdict['free'] = self.free
         jdict['mounted-on'] = self.mounted_on
 
+        jdict['is-available'] = self.is_available
+
         return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def is_available(self):
+        try:
+            os.listdir(self.mounted_on)
+            return True
+
+        except OSError:
+            return False
 
 
     # ----------------------------------------------------------------------------------------------------------------
@@ -115,13 +104,8 @@ class DiskVolume(JSONable):
 
 
     @property
-    def available(self):
-        return self.__available
-
-
-    @property
-    def use_percent(self):
-        return self.__use_percent
+    def free(self):
+        return self.__free
 
 
     @property
@@ -132,5 +116,55 @@ class DiskVolume(JSONable):
     # ----------------------------------------------------------------------------------------------------------------
 
     def __str__(self, *args, **kwargs):
-        return "DiskVolume:{filesystem:%s, size:%s, used:%s, available:%s, use_percent:%s, mounted_on:%s}" %  \
-               (self.filesystem, self.size, self.used, self.available, self.use_percent, self.mounted_on)
+        return "DiskVolume:{filesystem:%s, size:%s, used:%s, free:%s, mounted_on:%s}" %  \
+               (self.filesystem, self.size, self.used, self.free, self.mounted_on)
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class ReportedDiskVolume(DiskVolume):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def construct_from_jdict(cls, jdict):
+        if not jdict:
+            return None
+
+        filesystem = jdict.get('filesystem')
+        size = jdict.get('size')
+        used = jdict.get('used')
+        free = jdict.get('free')
+        mounted_on = jdict.get('mounted-on')
+
+        is_available = jdict.get('is-available')
+
+        return cls(filesystem, size, used, free, mounted_on, is_available)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, filesystem, size, used, free, mounted_on, is_available):
+        """
+        Constructor
+        """
+        super().__init__(filesystem, size, used, free, mounted_on)
+
+        self.__is_available = is_available                  # bool
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def is_available(self):
+        return self.__is_available
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "ReportedDiskVolume:{filesystem:%s, size:%s, used:%s, free:%s, mounted_on:%s, is_available:%s}" %  \
+               (self.filesystem, self.size, self.used, self.free, self.mounted_on, self.is_available)
