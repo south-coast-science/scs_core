8357d50d0d3fffab66c151ac2feab2b0e5939725
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-11-10 16:29:28 +0000

    Fixed Crypt

--- src/scs_core/aws/manager/s3_manager.py
+++ src/scs_core/aws/manager/s3_manager.py
@@ -10,7 +10,6 @@ from botocore.exceptions import ClientError
 
 from collections import OrderedDict
 
-from scs_core.data.crypt import Crypt
 from scs_core.data.datetime import LocalizedDatetime
 from scs_core.data.json import JSONable
 
@@ -178,17 +177,26 @@ class S3PersistenceManager(PersistenceManager):
 
 
     def load(self, dirname, filename, encryption_key=None):
+        if encryption_key:
+            from scs_core.data.crypt import Crypt
+
         key_name = self.__key_name(dirname, filename)
 
         text = self.__manager.retrieve_from_bucket(self.__BUCKET, key_name)
+
+        # noinspection PyUnboundLocalVariable
         jstr = text if encryption_key is None else Crypt.decrypt(encryption_key, text)
 
         return jstr
 
 
     def save(self, jstr, dirname, filename, encryption_key=None):
+        if encryption_key:
+            from scs_core.data.crypt import Crypt
+
         key_name = self.__key_name(dirname, filename)
 
+        # noinspection PyUnboundLocalVariable
         text = jstr + '\n' if encryption_key is None else Crypt.encrypt(encryption_key, jstr)
 
         self.__manager.put_object(text, self.__BUCKET, key_name)
