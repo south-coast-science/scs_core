517afd77018d506a5762fe52231929248209c854
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-12-28 18:03:02 +0000

    Added MessageRequest class

--- src/scs_core/aws/manager/lambda_message_manager.py
+++ src/scs_core/aws/manager/lambda_message_manager.py
@@ -16,6 +16,7 @@ from urllib.parse import urlparse, parse_qs
 from scs_core.aws.client.rest_client import RESTClient
 from scs_core.aws.data.message import Message
 
+from scs_core.data.datetime import LocalizedDatetime
 from scs_core.data.datum import Datum
 from scs_core.data.json import JSONable
 from scs_core.data.str import Str
@@ -62,18 +63,11 @@ class MessageManager(object):
         return None
 
 
-    def find_for_topic(self, topic, start_date, end_date, checkpoint, include_wrapper, rec_only):
+    def find_for_topic(self, topic, start_date, end_date, checkpoint, include_wrapper, _rec_only):
         request_path = '/default/AWSAggregate/'
         # request_path = '/topicMessages'
 
-        params = {self.__TOPIC:             topic,
-                  self.__START:             start_date.utc().as_iso8601(True),
-                  self.__END:               end_date.utc().as_iso8601(True),
-                  self.__INCLUDE_WRAPPER:   str(include_wrapper).lower(),
-                  self.__REC_ONLY:          str(rec_only).lower()}
-
-        if checkpoint:
-            params[self.__CHECKPOINT] = checkpoint
+        params = MessageRequest(topic, start_date, end_date, include_wrapper, False, checkpoint).params()
 
         # request...
         self.__rest_client.connect()
@@ -113,6 +107,108 @@ class MessageManager(object):
 
 # --------------------------------------------------------------------------------------------------------------------
 
+class MessageRequest(object):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def construct_from_params(cls, qsp):
+        if not qsp:
+            return None
+
+        topic = qsp.get('topic')
+        start = LocalizedDatetime.construct_from_iso8601(qsp.get('startTime'))
+        end = LocalizedDatetime.construct_from_iso8601(qsp.get('endTime'))
+
+        include_wrapper = qsp.get("includeWrapper", 'false') == 'true'
+        min_max = qsp.get("minMax", 'false') == 'true'
+        checkpoint = qsp.get('checkpoint')
+
+        if topic is None or start is None or end is None:
+            return None
+
+        if start > end:
+            return None
+
+        return cls(topic, start, end, include_wrapper, min_max, checkpoint)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, topic, start, end, include_wrapper, min_max, checkpoint):
+        """
+        Constructor
+        """
+        self.__topic = topic                                # string
+        self.__start = start                                # LocalizedDatetime
+        self.__end = end                                    # LocalizedDatetime
+
+        self.__include_wrapper = bool(include_wrapper)      # bool
+        self.__min_max = bool(min_max)                      # bool
+        self.__checkpoint = checkpoint                      # string
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def params(self):
+        params = {
+            'topic': self.topic,
+            'startTime': self.start.utc().as_iso8601(True),
+            'endTime': self.end.utc().as_iso8601(True),
+            'includeWrapper': str(self.include_wrapper).lower(),
+            'minMax': str(self.min_max).lower()
+        }
+
+        if self.checkpoint:
+            params['checkpoint'] =  self.checkpoint
+
+        return params
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def topic(self):
+        return self.__topic
+
+
+    @property
+    def start(self):
+        return self.__start
+
+
+    @property
+    def end(self):
+        return self.__end
+
+
+    @property
+    def include_wrapper(self):
+        return self.__include_wrapper
+
+
+    @property
+    def min_max(self):
+        return self.__min_max
+
+
+    @property
+    def checkpoint(self):
+        return self.__checkpoint
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "MessageResponse:{topic:%s, start:%s, end:%s, include_wrapper:%s, min_max:%s, checkpoint:%s}" % \
+               (self.topic, self.start, self.end, self.include_wrapper, self.min_max, self.checkpoint)
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
 class MessageResponse(JSONable):
     """
     classdocs
@@ -135,7 +231,7 @@ class MessageResponse(JSONable):
 
         next_url = jdict.get('next')
 
-        return MessageResponse(code, status, items, next_url)
+        return cls(code, status, items, next_url)
 
 
     # ----------------------------------------------------------------------------------------------------------------
