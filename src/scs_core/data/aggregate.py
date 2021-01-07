f8d2da55f502d8bca1d31d7dcbb16643b29f910f
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-01-07 15:48:17 +0000

    Built mashed vB

--- src/scs_core/data/aggregate.py
+++ src/scs_core/data/aggregate.py
@@ -12,7 +12,6 @@ from decimal import InvalidOperation
 
 from scs_core.data.categorical_regression import CategoricalRegression
 from scs_core.data.datetime import LocalizedDatetime
-from scs_core.data.datum import Datum
 from scs_core.data.linear_regression import LinearRegression
 from scs_core.data.path_dict import PathDict
 from scs_core.data.precision import Precision
