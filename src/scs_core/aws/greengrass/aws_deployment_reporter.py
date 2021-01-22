bac51bfc8d1c0dfa033055d6450383c797ffaef5
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-01-22 09:04:16 +0000

    Added AWSDeploymentReporter class

--- src/scs_core/aws/greengrass/aws_deployment_reporter.py
+++ src/scs_core/aws/greengrass/aws_deployment_reporter.py
@@ -2,20 +2,24 @@
 Created on 21 Jan 2021
 
 @author: Jade Page (jade.page@southcoastscience.com)
-
-
-
 """
+
+from scs_core.aws.data.deployment import Deployment
 from scs_core.aws.greengrass.aws_group import AWSGroup
-from scs_core.aws.greengrass.deployment_report import DeploymentReport
+
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class AWSDeployments(object):
+class AWSDeploymentReporter(object):
+
+    # ----------------------------------------------------------------------------------------------------------------
 
     def __init__(self, client, group_name=None):
         self.__group_name = group_name
         self.__client = client
+
+
+    # ----------------------------------------------------------------------------------------------------------------
 
     def get_all_groups(self):
         next_token = None
@@ -35,15 +39,13 @@ class AWSDeployments(object):
 
         return group_ids
 
+
     def get_groups(self, next_token):
         if next_token is None:
-            response = self.__client.list_groups()
-            return response
-        else:
-            response = self.__client.list_groups(
-                NextToken=next_token
-            )
-            return response
+            return self.__client.list_groups()
+
+        return self.__client.list_groups(NextToken=next_token)
+
 
     def get_deployments(self, group_ids):
         reports = []
@@ -55,9 +57,11 @@ class AWSDeployments(object):
                 if len(response["Deployments"]) > 0:
                     last_deployment = response["Deployments"][0]
                     group_name = self.get_group_name(id)
-                    reports.append(DeploymentReport.construct_from_aws_deployments(last_deployment, group_name))
+                    deployment = Deployment.construct_from_aws(group_name, last_deployment)
+                    reports.append(deployment)
 
         return reports
+
 
     def get_group_name(self, group_id):
         response = self.__client.get_group(
@@ -66,6 +70,7 @@ class AWSDeployments(object):
         if "Name" in response:
             return response["Name"]
 
+
     def get_group_id(self, group_name):
         aws_group_info = AWSGroup(group_name, self.__client)
 
@@ -73,3 +78,9 @@ class AWSDeployments(object):
         group_id = datum.node("GroupID")
 
         return group_id
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "AWSDeploymentReporter:{group_name:%s, client:%s}" % (self.__group_name, self.__client)
