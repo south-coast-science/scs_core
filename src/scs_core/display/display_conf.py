4bd4553326155564c68a85ff67e494f143146a05
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-01-27 17:16:09 +0000

    Refactored DisplayConf class

--- src/scs_core/display/display_conf.py
+++ src/scs_core/display/display_conf.py
@@ -1,42 +1,119 @@
 """
-Created on 27 Feb 2020
+Created on 21 Jun 2019
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
-a stub display configuration, to be implemented elsewhere
+example JSON:
+{"mode": "SYS", "device-name": "SCS Praxis/Handheld (dev)", "startup-message": "ON", "shutdown-message": "STANDBY",
+"show-time": true}
 """
+
+from collections import OrderedDict
+
+from scs_core.data.json import PersistentJSONable
 
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class DisplayConf(object):
+class DisplayConf(PersistentJSONable):
     """
     classdocs
     """
 
+    __MODES = ['SYS']
+
     @classmethod
     def modes(cls):
-        return []
+        return cls.__MODES
 
 
-    @classmethod
-    def load(cls, manager, encryption_key=None):
-        raise NotImplementedError
-
+    __FILENAME = "system_display_conf.json"
 
     @classmethod
-    def delete(cls, manager):
-        raise NotImplementedError
+    def persistence_location(cls):
+        return cls.conf_dir(), cls.__FILENAME
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def __init__(self, _mode, _device_name, _startup_message, _shutdown_message, _show_time):
+    @classmethod
+    def construct_from_jdict(cls, jdict):
+        if not jdict:
+            return None
+
+        mode = jdict.get('mode')
+
+        device_name = jdict.get('device-name')
+        startup_message = jdict.get('startup-message')
+        shutdown_message = jdict.get('shutdown-message')
+
+        show_time = jdict.get('show-time', True)
+
+        return cls(mode, device_name, startup_message, shutdown_message, show_time)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, mode, device_name, startup_message, shutdown_message, show_time):
         """
         Constructor
         """
-        raise NotImplementedError
+        self.__mode = mode
+
+        self.__device_name = device_name
+        self.__startup_message = startup_message
+        self.__shutdown_message = shutdown_message
+
+        self.__show_time = show_time
 
 
-    def save(self, manager, encryption_key=None):
-        raise NotImplementedError
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def mode(self):
+        return self.__mode
+
+
+    @property
+    def device_name(self):
+        return self.__device_name
+
+
+    @property
+    def startup_message(self):
+        return self.__startup_message
+
+
+    @property
+    def shutdown_message(self):
+        return self.__shutdown_message
+
+
+    @property
+    def show_time(self):
+        return self.__show_time
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        jdict = OrderedDict()
+
+        jdict['mode'] = self.mode
+
+        jdict['device-name'] = self.device_name
+        jdict['startup-message'] = self.startup_message
+        jdict['shutdown-message'] = self.shutdown_message
+
+        jdict['show-time'] = self.show_time
+
+        return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "DisplayConf(core):{mode:%s, device_name:%s, startup_message:%s, shutdown_message:%s, " \
+               "show_time:%s}" %  \
+               (self.mode, self.device_name, self.startup_message, self.shutdown_message,
+                self.show_time)
