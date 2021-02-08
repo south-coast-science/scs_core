0e61f253c11b5478dff1c779bc97e2cd394dd046
jade <jadempage@outlook.com>
2021-02-08 15:17:52 +0000

    V3 of aws group configuration files (needs testing)

--- src/scs_core/aws/greengrass/aws_group_configuration.py
+++ src/scs_core/aws/greengrass/aws_group_configuration.py
@@ -149,6 +149,7 @@ class AWSGroupConfigurator(object):
 
     __V1 = __CWD + "/v1"
     __V2 = __CWD + "/v2"
+    __V3 = __CWD + "/v3"
 
     # ----------------------------------------------------------------------------------------------------------------
 
@@ -199,7 +200,7 @@ class AWSGroupConfigurator(object):
     def define_aws_group_resources(self, host):
         # Setup default JSON
         if self.config.ml:
-            j_file = os.path.join(self.__V2, 'gg_resources_ml.json')
+            j_file = os.path.join(self.__V3, 'gg_resources_ml.json')
         else:
             j_file = os.path.join(self.__V1, 'gg_resources.json')
         with open(j_file) as f:
@@ -245,7 +246,7 @@ class AWSGroupConfigurator(object):
     def define_aws_group_functions(self):
         # Get template JSON
         if self.config.ml:
-            j_file = os.path.join(self.__V1, 'gg_functions_ml.json')
+            j_file = os.path.join(self.__V3, 'gg_functions_ml.json')
         else:
             j_file = os.path.join(self.__V1, 'gg_functions.json')
         with open(j_file) as f:
