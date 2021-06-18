1c281d9134069dd1b0210e85ef7eb402831bd35f
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-06-18 16:47:15 +0100

    Fixed socket parameter naming

--- src/scs_core/sys/node.py
+++ src/scs_core/sys/node.py
@@ -33,7 +33,7 @@ class Node(ABC):
 
     @staticmethod
     def ipv4_address():
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
         try:
             s.connect(('192.168.0.1', 1))  # host does not need to be reachable
