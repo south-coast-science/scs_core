c5d25fcccb6a6f4f56097ca099b28935ea89dad2
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-08-26 15:10:51 +0100

    Cleaning

--- tests/data/recurring_period_test.py
+++ tests/data/recurring_period_test.py
@@ -8,8 +8,8 @@ Created on 7 Jul 2021
 
 import json
 
-from scs_core.data.recurring_period import RecurringPeriod
 from scs_core.data.datetime import LocalizedDatetime
+from scs_core.data.recurring_period import RecurringPeriod
 from scs_core.data.json import JSONify
 
 
