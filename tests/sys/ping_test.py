3cde94035ea8dc5f0febf5b96262a344c9369109
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-12-25 12:54:28 +0000

    Added is_connected(..) method to Node class

---
+++ tests/sys/ping_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Created on 15 Dec 2021
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_core.sys.node import Node
+
+
+# --------------------------------------------------------------------------------------------------------------------
+# run...
+
+result = Node.ping('8.8.8.8', ttl=128)
+print("ping result: %s" % result)
