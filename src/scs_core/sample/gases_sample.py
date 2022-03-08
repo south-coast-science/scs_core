24778af62cf43973f9e2ade658ac05a778761670
Bruno Beloff <bruno.beloff@southcoastscience.com>
2022-03-08 17:50:06 +0000

    Fixed version number retrieval

--- src/scs_core/sample/gases_sample.py
+++ src/scs_core/sample/gases_sample.py
@@ -49,7 +49,11 @@ class GasesSample(Sample):
         # Sample...
         tag = jdict.get('tag')
         rec = LocalizedDatetime.construct_from_jdict(jdict.get('rec'))
-        version = jdict.get('ver', cls.DEFAULT_VERSION)
+
+        try:
+            version = float(jdict.get('ver'))
+        except (TypeError, ValueError):
+            version = cls.DEFAULT_VERSION
 
         src = jdict.get('src')
         val = jdict.get('val')
