f0570b45e2a2166b58d96898b831c7030f06e9ec
Bruno Beloff <bruno.beloff@southcoastscience.com>
2022-03-20 13:22:13 +0000

    Refactored SensorBaseline class

--- src/scs_core/gas/sensor_baseline.py
+++ src/scs_core/gas/sensor_baseline.py
@@ -28,47 +28,39 @@ class SensorBaseline(JSONable):
         if not jdict:
             return cls.null_datum()
 
-        if 'calibrated_on' in jdict:                            # TODO: deprecated
-            date = Datum.date(jdict.get('calibrated_on'))
+        if 'calibrated_on' in jdict:
+            date = Datum.date(jdict.get('calibrated_on'))                   # deprecated
             calibrated_on = LocalizedDatetime.construct_from_date(date)
 
         else:
             calibrated_on = LocalizedDatetime.construct_from_iso8601(jdict.get('calibrated-on'))
 
-        sample_on = LocalizedDatetime.construct_from_iso8601(jdict.get('sample-on'))
-        sample_humid = jdict.get('sample-hmd')
-        sample_temp = jdict.get('sample-tmp')
-
         offset = jdict.get('offset')
+        sample = SensorBaselineSample.construct_from_jdict(jdict.get('sample'))
 
-        return SensorBaseline(calibrated_on, sample_on, sample_humid, sample_temp, offset)
+        return cls(calibrated_on, offset, sample=sample)
 
 
     @classmethod
     def null_datum(cls):
-        return SensorBaseline(None, None, None, None, 0)
+        return SensorBaseline(None, 0)
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def __init__(self, calibrated_on, sample_on, sample_humid, sample_temp, offset):
+    def __init__(self, calibrated_on, offset, sample=None):
         """
         Constructor
         """
         self.__calibrated_on = calibrated_on                    # LocalizedDatetime
-
-        self.__sample_on = sample_on                            # LocalizedDatetime
-        self.__sample_humid = Datum.float(sample_humid, 1)      # float
-        self.__sample_temp = Datum.float(sample_temp, 1)        # float
-
-        self.__offset = int(offset)                             # int                       ppb
+        self.__offset = int(offset)                             # int                           ppb
+        self.__sample = sample                                  # SensorBaselineSample
 
 
     def __eq__(self, other):
         try:
-            return self.calibrated_on == other.calibrated_on and self.sample_on == other.sample_on and \
-                   self.sample_humid == other.sample_humid and self.sample_temp == other.sample_temp and \
-                   self.offset == other.offset
+            return self.calibrated_on == other.calibrated_on and self.offset == other.offset and \
+                   self.sample == other.sample
         except (TypeError, AttributeError):
             return False
 
@@ -78,13 +70,9 @@ class SensorBaseline(JSONable):
     def as_json(self):
         jdict = OrderedDict()
 
-        jdict['calibrated-on'] = None if self.calibrated_on is None else self.calibrated_on.as_iso8601(False)
-
-        jdict['sample-on'] = None if self.sample_on is None else self.sample_on.as_iso8601(False)
-        jdict['sample-hmd'] = self.sample_humid
-        jdict['sample-tmp'] = self.sample_temp
-
+        jdict['calibrated-on'] = None if self.calibrated_on is None else self.calibrated_on.as_iso8601()
         jdict['offset'] = self.offset
+        jdict['sample'] = self.sample
 
         return jdict
 
@@ -97,27 +85,91 @@ class SensorBaseline(JSONable):
 
 
     @property
-    def sample_on(self):
-        return self.__sample_on
-
-
-    @property
-    def sample_humid(self):
-        return self.__sample_humid
-
-
-    @property
-    def sample_temp(self):
-        return self.__sample_temp
-
-
-    @property
     def offset(self):
         return self.__offset
+
+
+    @property
+    def sample(self):
+        return self.__sample
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
     def __str__(self, *args, **kwargs):
-        return "SensorBaseline:{calibrated_on:%s, sample_on:%s, sample_humid:%s, sample_temp:%s, offset:%s}" %  \
-               (self.calibrated_on, self.sample_on, self.sample_humid, self.sample_temp, self.offset)
+        return "SensorBaseline:{calibrated_on:%s, offset:%s, sample:%s}" %  \
+               (self.calibrated_on, self.offset, self.sample)
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class SensorBaselineSample(JSONable):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def construct_from_jdict(cls, jdict):
+        if not jdict:
+            return None
+
+        rec = LocalizedDatetime.construct_from_iso8601(jdict.get('rec'))
+        humid = jdict.get('hmd')
+        temp = jdict.get('tmp')
+
+        return cls(rec, humid, temp)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, rec, humid, temp):
+        """
+        Constructor
+        """
+        self.__rec = rec                                # LocalizedDatetime
+        self.__humid = Datum.float(humid, 1)            # float
+        self.__temp = Datum.float(temp, 1)              # float
+
+
+    def __eq__(self, other):
+        try:
+            return self.rec == other.rec and self.humid == other.humid and self.temp == other.temp
+        except (TypeError, AttributeError):
+            return False
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        jdict = OrderedDict()
+
+        jdict['rec'] = None if self.rec is None else self.rec.as_iso8601()
+        jdict['hmd'] = self.humid
+        jdict['tmp'] = self.temp
+
+        return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def rec(self):
+        return self.__rec
+
+
+    @property
+    def humid(self):
+        return self.__humid
+
+
+    @property
+    def temp(self):
+        return self.__temp
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "SensorBaseline:{rec:%s, humid:%s, temp:%s}" %  (self.rec, self.humid, self.temp)
