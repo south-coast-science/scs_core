9e35b89bb1a2c4c3664d80199bd5b20e25f89929
Bruno Beloff <bruno.beloff@southcoastscience.com>
2022-07-04 11:43:39 +0100

    Refactored VCalAnalysis -> StatsAnalysis

--- src/scs_core/data/stats.py
+++ src/scs_core/data/stats.py
@@ -198,3 +198,154 @@ class Stats(JSONable):
     def __str__(self, *args, **kwargs):
         return "Stats:{minimum:%s, mean:%s, median:%s, maximum:%s, variance:%s, stdev:%s, stdev2:%s, stdev3:%s}" %  \
                (self.minimum, self.mean, self.median, self.maximum, self.variance, self.stdev, self.stdev2, self.stdev3)
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class StatsAnalysis(JSONable):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def construct_from_jdict(cls, jdict):
+        if not jdict:
+            return None
+
+        minimum = jdict.get('min')
+        maximum = jdict.get('max')
+
+        lower3 = jdict.get('l3')
+        lower2 = jdict.get('l2')
+        lower1 = jdict.get('l1')
+
+        upper1 = jdict.get('u1')
+        upper2 = jdict.get('u2')
+        upper3 = jdict.get('u3')
+
+        amp1 = jdict.get('a1')
+        amp2 = jdict.get('a2')
+        amp3 = jdict.get('a3')
+
+        return cls(minimum, maximum, lower3, lower2, lower1, upper1, upper2, upper3, amp1, amp2, amp3)
+
+
+    @classmethod
+    def construct_from_stats_jdict(cls, jdict):
+        stats = Stats.construct_from_jdict(jdict)
+
+        return cls(stats.minimum, stats.maximum, stats.lower3(), stats.lower2(), stats.lower1(),
+                   stats.upper1(), stats.upper2(), stats.upper3(), stats.amp1(), stats.amp2(), stats.amp3())
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, minimum, maximum, lower3, lower2, lower1, upper1, upper2, upper3, amp1, amp2, amp3):
+        """
+        Constructor
+        """
+        self.__minimum = round(float(minimum), 1)
+        self.__maximum = round(float(maximum), 1)
+
+        self.__lower3 = round(float(lower3), 1)
+        self.__lower2 = round(float(lower2), 1)
+        self.__lower1 = round(float(lower1), 1)
+
+        self.__upper1 = round(float(upper1), 1)
+        self.__upper2 = round(float(upper2), 1)
+        self.__upper3 = round(float(upper3), 1)
+
+        self.__amp1 = round(float(amp1), 1)
+        self.__amp2 = round(float(amp2), 1)
+        self.__amp3 = round(float(amp3), 1)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        jdict = OrderedDict()
+
+        jdict['min'] = self.minimum
+        jdict['max'] = self.maximum
+
+        jdict['l3'] = self.lower3
+        jdict['l2'] = self.lower2
+        jdict['l1'] = self.lower1
+
+        jdict['u1'] = self.upper1
+        jdict['u2'] = self.upper2
+        jdict['u3'] = self.upper3
+
+        jdict['a1'] = self.amp1
+        jdict['a2'] = self.amp2
+        jdict['a3'] = self.amp3
+
+        return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def minimum(self):
+        return self.__minimum
+
+
+    @property
+    def maximum(self):
+        return self.__maximum
+
+
+    @property
+    def lower3(self):
+        return self.__lower3
+
+
+    @property
+    def lower2(self):
+        return self.__lower2
+
+
+    @property
+    def lower1(self):
+        return self.__lower1
+
+
+    @property
+    def upper1(self):
+        return self.__upper1
+
+
+    @property
+    def upper2(self):
+        return self.__upper2
+
+
+    @property
+    def upper3(self):
+        return self.__upper3
+
+
+    @property
+    def amp1(self):
+        return self.__amp1
+
+
+    @property
+    def amp2(self):
+        return self.__amp2
+
+
+    @property
+    def amp3(self):
+        return self.__amp3
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "StatsAnalysis:{minimum:%s, maximum:%s, lower3:%s, lower2:%s, lower1:%s, " \
+               "upper1:%s, upper2:%s, upper3:%s, amp1:%s, amp2:%s, amp3:%s}" % \
+               (self.minimum, self.maximum, self.lower3, self.lower2, self.lower1,
+                self.upper1, self.upper2, self.upper3, self.amp1, self.amp2, self.amp3)
--- src/scs_core/model/gas/vcal_analysis.py
+++ src/scs_core/model/gas/vcal_analysis.py
@@ -10,7 +10,7 @@ Created on 27 Mar 2022
 from collections import OrderedDict
 
 from scs_core.data.json import JSONable
-from scs_core.data.stats import Stats
+from scs_core.data.stats import StatsAnalysis
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -35,7 +35,7 @@ class VCalAnalyses(JSONable):
                 offset = item
                 continue
 
-            analyses[key] = VCalAnalysis.construct_from_jdict(item)
+            analyses[key] = StatsAnalysis.construct_from_jdict(item)
 
         return cls(offset, analyses)
 
@@ -59,7 +59,7 @@ class VCalAnalyses(JSONable):
     # ----------------------------------------------------------------------------------------------------------------
 
     def append_stats(self, gas, jdict):
-        self.__analyses[gas] = VCalAnalysis.construct_from_stats_jdict(jdict)
+        self.__analyses[gas] = StatsAnalysis.construct_from_stats_jdict(jdict)
 
 
     # ----------------------------------------------------------------------------------------------------------------
@@ -91,154 +91,3 @@ class VCalAnalyses(JSONable):
 
     def __str__(self, *args, **kwargs):
         return "VCalAnalyses:{offset:%s, analyses:%s}" % (self.offset, self.analyses)
-
-
-# --------------------------------------------------------------------------------------------------------------------
-
-class VCalAnalysis(JSONable):
-    """
-    classdocs
-    """
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    @classmethod
-    def construct_from_jdict(cls, jdict):
-        if not jdict:
-            return None
-
-        minimum = jdict.get('min')
-        maximum = jdict.get('max')
-
-        lower3 = jdict.get('l3')
-        lower2 = jdict.get('l2')
-        lower1 = jdict.get('l1')
-
-        upper1 = jdict.get('u1')
-        upper2 = jdict.get('u2')
-        upper3 = jdict.get('u3')
-
-        amp1 = jdict.get('a1')
-        amp2 = jdict.get('a2')
-        amp3 = jdict.get('a3')
-
-        return cls(minimum, maximum, lower3, lower2, lower1, upper1, upper2, upper3, amp1, amp2, amp3)
-
-
-    @classmethod
-    def construct_from_stats_jdict(cls, jdict):
-        stats = Stats.construct_from_jdict(jdict)
-
-        return cls(stats.minimum, stats.maximum, stats.lower3(), stats.lower2(), stats.lower1(),
-                   stats.upper1(), stats.upper2(), stats.upper3(), stats.amp1(), stats.amp2(), stats.amp3())
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    def __init__(self, minimum, maximum, lower3, lower2, lower1, upper1, upper2, upper3, amp1, amp2, amp3):
-        """
-        Constructor
-        """
-        self.__minimum = round(float(minimum), 1)
-        self.__maximum = round(float(maximum), 1)
-
-        self.__lower3 = round(float(lower3), 1)
-        self.__lower2 = round(float(lower2), 1)
-        self.__lower1 = round(float(lower1), 1)
-
-        self.__upper1 = round(float(upper1), 1)
-        self.__upper2 = round(float(upper2), 1)
-        self.__upper3 = round(float(upper3), 1)
-
-        self.__amp1 = round(float(amp1), 1)
-        self.__amp2 = round(float(amp2), 1)
-        self.__amp3 = round(float(amp3), 1)
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    def as_json(self):
-        jdict = OrderedDict()
-
-        jdict['min'] = self.minimum
-        jdict['max'] = self.maximum
-
-        jdict['l3'] = self.lower3
-        jdict['l2'] = self.lower2
-        jdict['l1'] = self.lower1
-
-        jdict['u1'] = self.upper1
-        jdict['u2'] = self.upper2
-        jdict['u3'] = self.upper3
-
-        jdict['a1'] = self.amp1
-        jdict['a2'] = self.amp2
-        jdict['a3'] = self.amp3
-
-        return jdict
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    @property
-    def minimum(self):
-        return self.__minimum
-
-
-    @property
-    def maximum(self):
-        return self.__maximum
-
-
-    @property
-    def lower3(self):
-        return self.__lower3
-
-
-    @property
-    def lower2(self):
-        return self.__lower2
-
-
-    @property
-    def lower1(self):
-        return self.__lower1
-
-
-    @property
-    def upper1(self):
-        return self.__upper1
-
-
-    @property
-    def upper2(self):
-        return self.__upper2
-
-
-    @property
-    def upper3(self):
-        return self.__upper3
-
-
-    @property
-    def amp1(self):
-        return self.__amp1
-
-
-    @property
-    def amp2(self):
-        return self.__amp2
-
-
-    @property
-    def amp3(self):
-        return self.__amp3
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    def __str__(self, *args, **kwargs):
-        return "VCalAnalysis:{minimum:%s, maximum:%s, lower3:%s, lower2:%s, lower1:%s, " \
-               "upper1:%s, upper2:%s, upper3:%s, amp1:%s, amp2:%s, amp3:%s}" % \
-               (self.minimum, self.maximum, self.lower3, self.lower2, self.lower1,
-                self.upper1, self.upper2, self.upper3, self.amp1, self.amp2, self.amp3)
