325ac8c519ae3a07fd80ce8a81267099793a063f
Bruno Beloff <bruno.beloff@southcoastscience.com>
2022-11-28 11:44:07 +0000

    Cleaning

--- tests/sys/filesystem_report_test.py
+++ tests/sys/filesystem_report_test.py
@@ -6,6 +6,8 @@ Created on 25 Nov 2022
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 """
 
+import json
+
 from scs_core.csv.csv_logger_conf import CSVLoggerConf
 from scs_core.data.json import JSONify
 from scs_core.sys.filesystem import FilesystemReport
@@ -15,17 +17,18 @@ from scs_host.sys.host import Host
 
 # --------------------------------------------------------------------------------------------------------------------
 
-DATA_STORE = "/srv/removable_data_store"
-
-data_log_1 = FilesystemReport.construct(DATA_STORE)
-print("data_log_1: %s" % data_log_1)
-print(JSONify.dumps(data_log_1))
-print("-")
-
 conf = CSVLoggerConf.load(Host)
 print("conf: %s" % conf)
+print("-")
 
-data_log_2 = conf.filesystem_report()
+data_log_1 = conf.filesystem_report()
+print("data_log_1: %s" % data_log_1)
+jstr = JSONify.dumps(data_log_1)
+print(jstr)
+print("-")
+
+data_log_2 = FilesystemReport.construct_from_jdict(json.loads(jstr))
 print("data_log_2: %s" % data_log_2)
+print("-")
 
 print("equals: %s" % str(data_log_1 == data_log_2))
