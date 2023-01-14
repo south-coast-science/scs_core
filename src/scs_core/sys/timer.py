ad658b539ed5ce48c9ac6924c342fe29203c5be4
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-01-14 10:46:51 +0000

    Refactored Timer class

--- src/scs_core/sys/timer.py
+++ src/scs_core/sys/timer.py
@@ -21,19 +21,20 @@ class Timer(object):
         Constructor
         """
         self.__start_time = time()
+        self.__checkpoint_time = time()
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def elapsed(self):
+    def total(self):
         elapsed = time() - self.__start_time
 
         return round(elapsed, 3)
 
 
-    def check(self):
-        elapsed = time() - self.__start_time
-        self.__start_time = time()
+    def checkpoint(self):
+        elapsed = time() - self.__checkpoint_time
+        self.__checkpoint_time = time()
 
         return round(elapsed, 3)
 
@@ -41,4 +42,4 @@ class Timer(object):
     # ----------------------------------------------------------------------------------------------------------------
 
     def __str__(self, *args, **kwargs):
-        return "Timer:{start_time:%s}" % self.__start_time
+        return "Timer:{start_time:%s, checkpoint_time:%s}" % (self.__start_time, self.__checkpoint_time)
