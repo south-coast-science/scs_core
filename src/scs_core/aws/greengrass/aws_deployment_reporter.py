9b95809862c0059e1b6d0ab0fb6d2e4840c1e2ad
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-02-07 12:46:23 +0000

    cleaning

--- src/scs_core/aws/greengrass/aws_deployment_reporter.py
+++ src/scs_core/aws/greengrass/aws_deployment_reporter.py
@@ -50,7 +50,7 @@ class AWSDeploymentReporter(object):
 
     def get_group_names(self, group_ids, matching=None, currency=None):
         return list(deployment.group_name for deployment in
-                self.get_deployments(group_ids, matching=matching, currency=currency))
+                    self.get_deployments(group_ids, matching=matching, currency=currency))
 
 
     def get_deployments(self, group_ids, matching=None, currency=None):
