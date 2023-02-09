f06c4df430a2023c0314420f5847c77ed29af737
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-02-09 12:33:49 +0000

    Added CognitoPasswordManager class

--- src/scs_core/aws/security/cognito_user_manager.py
+++ src/scs_core/aws/security/cognito_user_manager.py
@@ -3,12 +3,15 @@ Created on 24 Nov 2021
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 """
-
+import logging
 from http import HTTPStatus
 
 from scs_core.aws.security.cognito_user import CognitoUserIdentity
+
 from scs_core.data.json import JSONify
+
 from scs_core.sys.http_exception import HTTPException
+from scs_core.sys.logging import Logging
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -120,3 +123,89 @@ class CognitoUserDeleter(object):
 
     def __str__(self, *args, **kwargs):
         return "CognitoUserDeleter:{id_token:%s}" % self.__id_token
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class CognitoPasswordManager(object):
+    """
+    classdocs
+    """
+
+    __URL = "https://df46l72wl7.execute-api.us-west-2.amazonaws.com/default/CognitoUserPassword"
+
+    __HEADERS = {'Content-type': 'application/x-www-form-urlencoded',
+                 'Accept': 'text/plain', 'Authorization': '@southcoastscience.com'}
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, http_client):
+        self.__http_client = http_client                # requests package
+        self.__logger = Logging.getLogger()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def resend_confirmation(self, email):
+        url = '/'.join((self.__URL, 'resend-confirmation'))
+        payload = {'email': email}
+
+        response = self.__http_client.post(url, headers=self.__HEADERS, data=JSONify.dumps(payload))
+        status = HTTPStatus(response.status_code)
+
+        logging.info('resend_confirmation - status_code: %s' % response.status_code)
+        logging.info('resend_confirmation - text: %s' % response.text)
+
+        if status != HTTPStatus.OK:
+            raise HTTPException.construct(status.value, response.reason, response.json())
+
+
+    def resend_temporary_password(self, email):
+        url = '/'.join((self.__URL, 'resend-temporary'))
+        payload = {'email': email}
+
+        response = self.__http_client.post(url, headers=self.__HEADERS, data=JSONify.dumps(payload))
+        status = HTTPStatus(response.status_code)
+
+        logging.info('resend_temporary_password - status_code: %s' % response.status_code)
+        logging.info('resend_temporary_password - text: %s' % response.text)
+
+        if status != HTTPStatus.OK:
+            raise HTTPException.construct(status.value, response.reason, response.json())
+
+
+    def request_reset_password(self, email):
+        url = '/'.join((self.__URL, 'request'))
+        payload = {'email': email}
+
+        response = self.__http_client.post(url, headers=self.__HEADERS, data=JSONify.dumps(payload))
+        status = HTTPStatus(response.status_code)
+
+        logging.info('request_reset_password - status_code: %s' % response.status_code)
+        logging.info('request_reset_password - text: %s' % response.text)
+
+        if status != HTTPStatus.OK:
+            raise HTTPException.construct(status.value, response.reason, response.json())
+
+
+    def do_reset_password(self, email, code, password):
+        url = '/'.join((self.__URL, 'reset'))
+        payload = {'email': email, 'reset_code': code, 'new_password': password}
+
+        response = self.__http_client.post(url, headers=self.__HEADERS, data=JSONify.dumps(payload))
+        status = HTTPStatus(response.status_code)
+
+        logging.info('do_reset_password - status_code: %s' % response.status_code)
+        logging.info('do_reset_password - text: %s' % response.text)
+
+        if status != HTTPStatus.OK:
+            raise HTTPException.construct(status.value, response.reason, response.json())
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "CognitoPasswordManager:{}"
+
+
