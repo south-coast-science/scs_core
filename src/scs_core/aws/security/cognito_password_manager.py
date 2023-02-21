58bea6bc33a8789cad2bf4f9c8ad85a0924c9d89
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-02-21 12:29:52 +0000

    Refactored CognitoLoginManager

--- src/scs_core/aws/security/cognito_password_manager.py
+++ src/scs_core/aws/security/cognito_password_manager.py
@@ -3,6 +3,12 @@ Created on 9 Feb 2022
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
+* resend_confirmation - re-send confirmation email when account is created using the API
+* resend_temporary_password - re-send temporary password that was created by the security_import system
+* request_reset_password - re-send a code needed to do a password reset
+* do_reset_password - perform a password reset using the code
+* do_set_password - change password when in force reset mode created by security_import system
+
 https://docs.python.org/3/howto/enum.html#planet
 
 document example:
@@ -84,15 +90,29 @@ class CognitoPasswordManager(object):
             raise HTTPException.construct(status.value, response.reason, response.json())
 
 
-    def do_reset_password(self, email, code, password):
+    def do_reset_password(self, email, code, new_password):
         url = '/'.join((self.__URL, 'reset'))
-        payload = {'email': email, 'reset_code': code, 'new_password': password}
+        payload = {'email': email, 'reset_code': code, 'new_password': new_password}
 
         response = self.__http_client.post(url, headers=self.__HEADERS, data=JSONify.dumps(payload))
         status = HTTPStatus(response.status_code)
 
         logging.info('do_reset_password - status_code: %s' % response.status_code)
         logging.info('do_reset_password - text: %s' % response.text)
+
+        if status != HTTPStatus.OK:
+            raise HTTPException.construct(status.value, response.reason, response.json())
+
+
+    def do_set_password(self, email, new_password, session):
+        url = '/'.join((self.__URL, 'respond'))
+        payload = {'email': email, 'new_password': new_password, 'session': session}
+
+        response = self.__http_client.post(url, headers=self.__HEADERS, data=JSONify.dumps(payload))
+        status = HTTPStatus(response.status_code)
+
+        logging.info('do_set_password - status_code: %s' % response.status_code)
+        logging.info('do_set_password - text: %s' % response.text)
 
         if status != HTTPStatus.OK:
             raise HTTPException.construct(status.value, response.reason, response.json())
