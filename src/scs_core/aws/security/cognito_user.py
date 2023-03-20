c743b5eafd11eaa8f4898340e50a867fce51548d
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-03-20 12:45:58 +0000

    Added CognitoClientUser class

--- src/scs_core/aws/security/cognito_user.py
+++ src/scs_core/aws/security/cognito_user.py
@@ -20,106 +20,30 @@ example AWS response:
 "is-tester": false, "created": "2023-03-07T15:32:17Z", "last-updated": "2023-03-08T14:12:00Z"}
 """
 
-import json
 import re
-import sys
-import termios
 
 from collections import OrderedDict
-from getpass import getpass
 
 from scs_core.data.datetime import LocalizedDatetime
 from scs_core.data.datum import Datum
-from scs_core.data.json import JSONable, MultiPersistentJSONable
+from scs_core.data.json import JSONable
 
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class CognitoUserCredentials(MultiPersistentJSONable):
+class CognitoUserCredentials(object):
     """
     classdocs
     """
 
-    __FILENAME = "cognito_user_credentials.json"
-
     # ----------------------------------------------------------------------------------------------------------------
 
-    @classmethod
-    def from_stdin(cls, name):
-        line = sys.stdin.readline()
-        jdict = json.loads(line)
-
-        return cls.construct_from_jdict(jdict, name=name)
-
-
-    @classmethod
-    def from_user(cls, name, existing_email=None):
-        try:
-            termios.tcflush(sys.stdin, termios.TCIOFLUSH)               # flush stdin
-        except termios.error:
-            pass
-
-        if existing_email:
-            email = existing_email
-
-        else:
-            print("Enter email address: ", end="", file=sys.stderr)
-            email = input().strip()
-
-        print("Enter password: ", end="", file=sys.stderr)
-        password = input().strip()
-
-        print("Enter retrieval password (RETURN for same): ", end="", file=sys.stderr)
-        retrieval_password = input().strip()
-
-        if not retrieval_password:
-            retrieval_password = password
-
-        return cls(name, email, password, retrieval_password)
-
-
-    @staticmethod
-    def password_from_user():
-        try:
-            termios.tcflush(sys.stdin, termios.TCIOFLUSH)               # flush stdin
-        except termios.error:
-            pass
-
-        return getpass().strip()
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    @classmethod
-    def persistence_location(cls, name):
-        filename = cls.__FILENAME if name is None else '_'.join((name, cls.__FILENAME))
-
-        return cls.aws_dir(), filename
-
-
-    @classmethod
-    def construct_from_jdict(cls, jdict, name=None, skeleton=False):
-        if not jdict:
-            return cls(None, None, None, None) if skeleton else None
-
-        email = jdict.get('email')
-        password = jdict.get('password')
-        retrieval_password = jdict.get('retrieval-password')
-
-        return cls(name, email, password, retrieval_password)
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    def __init__(self, name, email, password, retrieval_password):
+    def __init__(self, email, password):
         """
         Constructor
         """
-        super().__init__(name)
-
         self.__email = email                                        # string (email)
         self.__password = password                                  # string (AWS password)
-        self.__retrieval_password = retrieval_password              # string
 
 
     # ----------------------------------------------------------------------------------------------------------------
@@ -139,18 +63,6 @@ class CognitoUserCredentials(MultiPersistentJSONable):
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def as_json(self):
-        jdict = OrderedDict()
-
-        jdict['email'] = self.email
-        jdict['password'] = self.password
-        jdict['retrieval-password'] = self.retrieval_password
-
-        return jdict
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
     @property
     def email(self):
         return self.__email
@@ -166,16 +78,10 @@ class CognitoUserCredentials(MultiPersistentJSONable):
         return self.__password
 
 
-    @property
-    def retrieval_password(self):
-        return self.__retrieval_password
-
-
     # ----------------------------------------------------------------------------------------------------------------
 
     def __str__(self, *args, **kwargs):
-        return "CognitoUserCredentials:{name:%s, email:%s, password:%s, retrieval_password:%s}" %  \
-               (self.name, self.email, self.password, self.retrieval_password)
+        return "CognitoUserCredentials:{email:%s, password:%s}" %  (self.email, self.password)
 
 
 # --------------------------------------------------------------------------------------------------------------------
