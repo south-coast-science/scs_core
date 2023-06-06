3fd40a436cb75b942963ebaaa618e075994ecf22
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-06-06 11:03:30 +0100

    Updated AlertSpecificationManagerRequest

--- src/scs_core/aws/manager/alert_specification_manager.py
+++ src/scs_core/aws/manager/alert_specification_manager.py
@@ -33,8 +33,8 @@ class AlertSpecificationManager(APIClient):
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def find(self, token, topic_filter, path_filter, creator_filter):
-        request = AlertSpecificationManagerRequest(topic_filter, path_filter, creator_filter)
+    def find(self, token, description_filter, topic_filter, path_filter, creator_filter):
+        request = AlertSpecificationManagerRequest(description_filter, topic_filter, path_filter, creator_filter)
 
         response = self._http_client.get(self.__URL, headers=self._token_headers(token), params=request.params())
         self._check_response(response)
@@ -95,6 +95,7 @@ class AlertSpecificationManagerRequest(object):
     classdocs
     """
 
+    DESCRIPTION_FILTER = 'description'
     TOPIC_FILTER = 'topic'
     PATH_FILTER = 'path'
     CREATOR_FILTER = 'creator'
@@ -106,19 +107,21 @@ class AlertSpecificationManagerRequest(object):
         if not qsp:
             return None
 
+        description_filter = qsp.get(cls.DESCRIPTION_FILTER)
         topic_filter = qsp.get(cls.TOPIC_FILTER)
         path_filter = qsp.get(cls.PATH_FILTER)
         creator_filter = qsp.get(cls.CREATOR_FILTER)
 
-        return cls(topic_filter, path_filter, creator_filter)
+        return cls(description_filter, topic_filter, path_filter, creator_filter)
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def __init__(self, topic_filter, path_filter, creator_filter):
+    def __init__(self, description_filter, topic_filter, path_filter, creator_filter):
         """
         Constructor
         """
+        self.__description_filter = description_filter              # string
         self.__topic_filter = topic_filter                          # string
         self.__path_filter = path_filter                            # string
         self.__creator_filter = creator_filter                      # string
@@ -134,7 +137,10 @@ class AlertSpecificationManagerRequest(object):
     # ----------------------------------------------------------------------------------------------------------------
 
     def params(self):
+        description_filter = self.description_filter if self.description_filter else None
+
         params = {
+            self.DESCRIPTION_FILTER: description_filter,
             self.TOPIC_FILTER: self.topic_filter,
             self.PATH_FILTER: self.path_filter,
             self.CREATOR_FILTER: self.creator_filter
@@ -144,6 +150,11 @@ class AlertSpecificationManagerRequest(object):
 
 
     # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def description_filter(self):
+        return self.__description_filter
+
 
     @property
     def topic_filter(self):
@@ -163,8 +174,10 @@ class AlertSpecificationManagerRequest(object):
     # ----------------------------------------------------------------------------------------------------------------
 
     def __str__(self, *args, **kwargs):
-        return "AlertSpecificationManagerRequest:{topic_filter:%s, path_filter:%s, creator_filter:%s}" % \
-               (self.topic_filter, self.path_filter, self.creator_filter)
+        return "AlertSpecificationManagerRequest:{description_filter:%s, topic_filter:%s, path_filter:%s, " \
+               "creator_filter:%s}" % \
+               (self.description_filter, self.topic_filter, self.path_filter,
+                self.creator_filter)
 
 
 # --------------------------------------------------------------------------------------------------------------------
