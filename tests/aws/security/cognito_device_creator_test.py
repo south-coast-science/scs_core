fd9d1d5c8dd298120540851448f4bc586ae2ccbf
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-06-27 11:21:06 +0100

    Cleaning

--- tests/aws/security/cognito_device_creator_test.py
+++ tests/aws/security/cognito_device_creator_test.py
@@ -8,7 +8,7 @@ Created on 24 Apr 2023
 
 import requests
 
-from scs_core.aws.security.cognito_device import CognitoDeviceCredentials
+from scs_core.aws.security.cognito_device import CognitoDeviceIdentity
 from scs_core.aws.security.cognito_device_creator import CognitoDeviceCreator
 
 from scs_core.data.json import JSONify
@@ -18,8 +18,9 @@ from scs_core.data.json import JSONify
 
 tag = 'scs-test-1'
 shared_secret = '0123456789abcdef'
+invoice_number = 'INV-TEST001'
 
-credentials = CognitoDeviceCredentials(tag, shared_secret)
+credentials = CognitoDeviceIdentity(tag, shared_secret, invoice_number, None, None)
 print(credentials)
 print("-")
 
