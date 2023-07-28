29bbbc2f3f4e241c1f7ce513dab608dd1906dc21
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-07-28 14:13:11 +0100

    Cleaning

--- src/scs_core/aws/security/cognito_client_credentials.py
+++ src/scs_core/aws/security/cognito_client_credentials.py
@@ -10,6 +10,7 @@ example document:
 """
 
 import json
+import os
 import sys
 import termios
 
@@ -32,6 +33,7 @@ class CognitoClientCredentials(CognitoUserCredentials, MultiPersistentJSONable):
     """
 
     __FILENAME = "cognito_user_credentials.json"
+    __ENV_PASSWORD = 'SCS_CREDENTIALS_RETRIEVAL'
 
     # ----------------------------------------------------------------------------------------------------------------
 
@@ -40,13 +42,17 @@ class CognitoClientCredentials(CognitoUserCredentials, MultiPersistentJSONable):
         logger = Logging.getLogger()
 
         if not cls.exists(host, name=name):
+            logger.info("name: [%s]" % name)
             logger.error("Cognito credentials not available.")
             return None
 
         try:
+            password = os.environ[cls.__ENV_PASSWORD]
+        except KeyError:
             password = cls.password_from_user()
-            return CognitoClientCredentials.load(host, name=name, encryption_key=password)
 
+        try:
+            return CognitoClientCredentials.load(host, name=name, encryption_key=password)
         except (KeyError, ValueError):
             logger.error("incorrect password.")
             return None
