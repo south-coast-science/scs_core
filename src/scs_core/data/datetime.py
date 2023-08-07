46ed97fed557a47a016d3f07d8e7a1fcae18c415
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-08-07 09:35:15 +0100

    Fixed Date validation

--- src/scs_core/data/datetime.py
+++ src/scs_core/data/datetime.py
@@ -474,12 +474,15 @@ class Date(object):
 
     @classmethod
     def is_valid_iso_format(cls, date_str):
-        try:
-            match = re.match(r'.*\d{4}-\d{2}-\d{2}.*', date_str)          # e.g. 2023-03-15
-            return match is not None
+        match = re.match(r'\d{4}-\d{2}-\d{2}', date_str)  # e.g. 2023-03-15
+        return match is not None
 
-        except (TypeError, ValueError):
-            return False
+        # try:
+        #     match = re.match(r'.*\d{4}-\d{2}-\d{2}.*', date_str)          # e.g. 2023-03-15
+        #     return match is not None
+        #
+        # except (TypeError, ValueError):
+        #     return False
 
 
 # --------------------------------------------------------------------------------------------------------------------
