db8c2aa0fc5455e2ee682e36cf3f3ed93c772b25
Bruno Beloff <bruno.beloff@southcoastscience.com>
2024-04-09 15:56:00 +0100

    Added aws_topic_origin.py CLU

--- src/scs_core/aws/manager/topic_origin/topic_origin_intercourse.py
+++ src/scs_core/aws/manager/topic_origin/topic_origin_intercourse.py
@@ -4,7 +4,7 @@ Created on 9 Apr 2024
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
 example document:
-{"topic": "acsoft/uk-urban/loc/960/particulates", "device": "scs-bgx-960", "rec": "2024-03-27T15:51:54Z"}
+{"topic": "south-coast-science-dev/development/loc/1/climate", "device": "scs-be2-3", "rec": "2023-03-14T00:00:03Z"}
 """
 
 from collections import OrderedDict
