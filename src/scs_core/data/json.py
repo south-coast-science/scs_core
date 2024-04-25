70d839a32af4a5732ad27db50d544fb15219f3f2
Bruno Beloff <bruno.beloff@southcoastscience.com>
2024-04-25 09:56:26 +0100

    JSONify experiements

--- src/scs_core/data/json.py
+++ src/scs_core/data/json.py
@@ -61,8 +61,18 @@ class JSONify(json.JSONEncoder):
         if isinstance(obj, Decimal):
             return float(obj) if Datum.is_float(str(obj)) else int(obj)
 
-        return json.JSONEncoder.default(self, obj)
+        return JSONKWEncoder.default(self, obj)
 
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class JSONKWEncoder(json.JSONEncoder):
+    def __init__(self, *, skipkeys=False, ensure_ascii=True,
+                 check_circular=True, allow_nan=True, sort_keys=False,
+                 indent=None, separators=None, default=None):
+        super().__init__(skipkeys=skipkeys, ensure_ascii=ensure_ascii,
+                         check_circular=check_circular, allow_nan=allow_nan, sort_keys=sort_keys,
+                         indent=indent, separators=separators, default=default)
 
 # --------------------------------------------------------------------------------------------------------------------
 
